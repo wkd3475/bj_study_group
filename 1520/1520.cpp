8910499f11f4b2b5e761cfcd1fef1c2b1e12495d
wkd3475 <wkd3475@gmail.com>
2019-12-23 13:17:11 +0900

    2019/12/23

--- 1520/1520.cpp
+++ 1520/1520.cpp
@@ -3,9 +3,33 @@
 using namespace std;
 
 int inputArray[501][501];
+int dp[501][501]={0,};
+int visited[501][501]={0,};
 int action[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-void 
+int findWays(int height, int width, int max_h, int max_w) {
+    if (visited[height][width]==1)
+        return dp[height][width];
+
+    if (height==0&&width==0)
+        return 1;
+    
+    for (int i=0; i<4; i++) {
+        if (height+action[i][0]<0||height+action[i][0]>=max_h)
+            continue;
+
+        if (width+action[i][1]<0||width+action[i][1]>=max_w)
+            continue;
+
+        if (inputArray[height][width] < inputArray[height+action[i][0]][width+action[i][1]]) {
+            dp[height][width] += findWays(height+action[i][0], width+action[i][1], max_h, max_w);
+        }
+    }
+
+    visited[height][width]=1;
+
+    return dp[height][width];
+}
 
 int main() {
     int height, width;
@@ -17,23 +41,12 @@ int main() {
         }
     }
 
-    // for (int i=0; i<=height; i++) {
-    //     inputArray[i][width] = INT32_MAX;
-    // }
-
-    // for (int i=0; i<=width; i++) {
-    //     inputArray[height][i] = INT32_MAX;
-    // }
-
-    findWays(height, width);
-
-    for (int i=height-1; i>0; i--) {
-        for (int j=width-1; j>0; j--) {
-
+    for (int i=height-1; i>=0; i--) {
+        for (int j=width-1; j>=0; j--) {
+            findWays(i, j, height, width);
         }
     }
 
-
-
+    printf("%d\n", dp[height-1][width-1]);
     return 0;
 }
